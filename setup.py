eee4ace13891280e0ae6c0051e22f7f039523861
Sixto Martin <pitbulk@gmail.com>
2018-06-08 13:35:35 +0200

    Update dependencies: OneLogin Python SDK and Boto3

--- setup.py
+++ setup.py
@@ -34,8 +34,8 @@ setup(
         '': 'src',
     },
     install_requires=[
-        'boto3==1.4.7',
-        'onelogin==1.1.0',
+        'boto3==1.7.33',
+        'onelogin==1.3.2',
         'optparse-pretty==0.1.1'
     ],
     test_suite='tests',
